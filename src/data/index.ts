ff6385d79a7896e0c155d1ab9861339936dcb994
manohySr <manohyrajaonah@gmail.com>
2025-06-02 14:42:22 +0300

    add data projects

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,14 +2,14 @@ import type { Project, Technology } from '../types';
 
 export const projects: Project[] = [
   {
-    name: 'Nasa Launch System API',
+    name: '🚀 Nasa Launch System API',
     description:
       'Built an API for Nasa Launch System using Node.js with MVC architecture, writing integration tests with Jest and Supertest, and automating workflows with GitHub Actions.',
     technologies: ['Node.js', 'Jest', 'Mongoose', 'GitHubActions'],
     github: 'https://github.com/manohySr/NasaTest',
   },
   {
-    name: 'Cat Wiki Master',
+    name: '😼 Cat Wiki Master',
     description:
       'Built a Fullstack app using Next.js 14 with TypeScript, featuring pages for individual cat profiles, top 10 cats, and dynamic API routes to fetch cat data. Integrated Tailwind CSS for styling and implemented reusable components for the UI.',
     technologies: ['Next.js 14', 'TypeScript', 'Tailwind', 'MongoDb'],
@@ -17,13 +17,28 @@ export const projects: Project[] = [
     live: 'https://cat-wiki-master-seven.vercel.app',
   },
   {
-    name: 'Kotrana CSS Part 3',
+    name: '🌺 Kotrana CSS Part 3',
     description:
       'A CSS practice project with various stylesheets and SASS compilation techniques. Utilized SASS for better organization and modularity of styles. Demonstrates CSS prefixing, concatenation, and the use of SVG icons with Icomoon integration.',
     technologies: ['HTML', 'CSS', 'Sass', 'SVG', 'BEM'],
     github: 'https://github.com/manohySr/kotrana-css-part-3',
     live: 'https://kotrana-part-3.netlify.app',
   },
+  {
+    name: '🎬 TMDB CLI Tool',
+    description:
+      'A beautiful command-line interface tool to fetch and display movie information from The Movie Database (TMDB) API.',
+    technologies: ['Golang', 'Go Cobra', 'TMDB API'],
+    github: 'https://github.com/manohySr/tmdb-app',
+  },
+  {
+    name: '⏳ GitHub Activity CLI',
+    description:
+      'A command-line tool to track and analyze GitHub activity. Fetch the recent activity of a GitHub user and display it in the terminal.',
+    technologies: ['Python', 'Typer', 'Rich', 'Github API'],
+    github: 'https://github.com/manohySr/github-activity',
+  },
+  
 ];
 
 export const technologies: Technology[] = [
